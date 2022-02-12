3204f75c855be4bfbf3f56788473c116e3fd1a3a
Luis Serrano <84140881+LuisBy860@users.noreply.github.com>
2022-02-11 19:30:33 -0600

    Agregar archivos de proyecto.

---
+++ practica1/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using practica1.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace practica1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult pratica (Double DatoN1, Double DatoN2)
+        {
+
+            Double resultado = DatoN1 + DatoN2;
+            ViewBag.DatoN1V = DatoN1;
+            ViewBag.DatoN2V = DatoN2;
+            ViewBag.VariableAEnviar = resultado;
+            return View();
+        }
+        public IActionResult resta(Double DatoN1, Double DatoN2)
+        {
+
+            Double resultado = DatoN1 - DatoN2;
+            ViewBag.DatoN1V = DatoN1;
+            ViewBag.DatoN2V = DatoN2;
+            ViewBag.VariableAEnviar = resultado;
+            return View();
+        }
+        public IActionResult multiplicacion(Double DatoN1, Double DatoN2)
+        {
+
+            Double resultado = DatoN1 * DatoN2;
+            ViewBag.DatoN1V = DatoN1;
+            ViewBag.DatoN2V = DatoN2;
+            ViewBag.VariableAEnviar = resultado;
+            return View();
+        }
+        public IActionResult division(Double DatoN1, Double DatoN2)
+        {
+            Double resultado = DatoN1 / DatoN2;
+            ViewBag.DatoN1V = DatoN1;
+            ViewBag.DatoN2V = DatoN2;
+            ViewBag.VariableAEnviar = resultado;
+
+            if (DatoN1>0 && DatoN2>0)
+            {
+                return View();
+               
+            }
+            return Redirect("error");
+
+        }
+        public IActionResult error()
+        {
+
+            return View();
+        }
+           
+
+    }
+}
